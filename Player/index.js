8e8d09aa80cb50c656d2cfc39759188c97f15aef
Alfredo <fredy_rios0123@outlook.com>
2022-10-18 17:05:12 +0000

    Creating a new branch

--- Player/index.js
+++ Player/index.js
@@ -2,7 +2,16 @@
 
 const io = require('socket.io-client');
 
+// Create a Constant variable to link to the server.
 const url = process.env.SERVER_URL || 'http://localhost:3002/hangout';
 
+// Create a Constant variable that will connect the "Socket.io" to the URL
 const socket = io.connect(url);
+
+// Create a "Socket.io" function that will start the Player's logic
+// The player must first log-in in order to activate the Server
+socket.io('playerJoin', (payload) => {
+  console.log('LET ME IN!!!')
+  console.log(payload);
+});
 
--- Server/server.js
+++ Server/server.js
@@ -6,4 +6,11 @@ const io = require('socket.io-client');
 const eventPool = require('./eventPool.js');
 const PORT = process.env.PORT || 3002;
 const server = io(PORT);
+const player = server.of('./player');
+console.log(`Server is listening on port: ${PORT}/player`);
+
+// Run whenever a new player has joined the room to play
+player.on('connection', (socket) => {
+  console.log('New Player has joined! ', socket.id);
+});
 
